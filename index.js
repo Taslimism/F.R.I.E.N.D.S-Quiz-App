a149e9ed6062153a7c0b539491556535fbf593ce
Taslim Raza Raza <>
2021-04-23 18:05:34 +0000

    Added options as key value pair

--- index.js
+++ index.js
@@ -4,48 +4,52 @@ var colors = require('colors/safe');
 
 const questionsAnswers = [{
   question : `On the episode "the one with the truth about London", with who Monica wanted to stay in first place?`,
-  option :`
-          a) Joey
-          b) Paulo
-          c) Richard
-          d) Chandler`,
-            
+  option :{
+          a : `Joey Tribbiani`,
+          b : `Paulo`,
+          c : `Richard`,
+          d : `Chandler Bing`
+  },
   answer : 'a'
 },
 {
   question : `What the coffee house used to be before?`,
-  option :` 
-            a) A coffee house
-            b) A supermarket
-            c) A bar
-            d) A xerox place `,
+  option :{
+            a : `A coffee house`,
+            b : `A supermarket`,
+            c : `A bar`,
+            d : `A xerox place`, 
+  },
   answer : 'c'
 },
 {
-  question : `Why Phoebe and Gary's relationship didn't work out?`,
-  option : `
-            a) Because Gary shot a cat
-            b) Because Gary shot a bird
-            c) Because Phoebe didn't loved Gary
-            d) Because Gary asked Phoebe to move in `,
+question : `Why Phoebe and Gary's relationship didn't work out?`,
+option : {
+            a : `Because Gary shot a cat`,
+            b : `Because Gary shot a bird`,
+            c : `Because Phoebe didn't loved Gary`,
+            d : `Because Gary asked Phoebe to move in` 
+  },
   answer : 'b'
 },
 {
-  question : `What's the name of the person Rachel gives up to marry?`,
-  option : `
-            a) Barry
-            b) Hugsy
-            c) Mike
-            d) Mark `,
+question : `What's the name of the person Rachel gives up to marry?`,
+option : {
+            a : `Barry`,
+            b : `Hugsy`,
+            c : `Mike`,
+            d : `Mark` 
+  },
   answer : 'a'
 },
 {
   question : `What is the name of Chandler's father's Las Vegas all-male burlesque?`,
-  option : `
-            a) Viva Gaygas
-            b) Viva Las Gaygas
-            c) Viva the Gaygas
-            d) Viva Las Vegas `,
+  option : {
+            a : 'Viva Gaygas',
+            b : 'Viva Las Gaygas',
+            c : 'Viva the Gaygas',
+            d : 'Viva Las Vegas' 
+},
   answer : 'b'
 }]
 
@@ -55,7 +59,11 @@ const play = (questionsAnswers) => {
   questionsAnswers.forEach((questionAnswer) => {
 
       console.log(questionAnswer.question);
-      console.log(questionAnswer.option);
+
+      Object.keys(questionAnswer.option).forEach((key)=>{
+          console.log(`${key} : ${questionAnswer.option[key]}`);
+        });
+      
       console.log();
       let ans = readLineSync.question("Your Answer? ")
       
@@ -63,8 +71,8 @@ const play = (questionsAnswers) => {
         console.log(colors.bgRed('Please input a b c or d.'));
         console.log();
         ans = readLineSync.question("Your Answer? ");
-        
       }
+
       if(questionAnswer.answer === ans) {
             console.log(colors.green("Congratulations! Correct Response."));
             score++;
